5b3406c866038414911fba5b1a9288735849c863
Mikhail Voroshilov <m.s.voroshilovv@gmail.com>
2021-12-25 14:37:07 +0800

    New programm

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 21000;
+        byte amountCurrency = 20;
+        int bonus_Miles = price / amountCurrency;
+        System.out.println(bonus_Miles);
+    }
+}
